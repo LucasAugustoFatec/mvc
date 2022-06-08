a98845209d1c10ac538704a24f1b08317853f6d0
LucasAugustoFatec <106602796+LucasAugustoFatec@users.noreply.github.com>
2022-06-08 08:40:43 -0300

    Update init.php

--- init.php
+++ init.php
@@ -7,8 +7,10 @@ date_default_timezone_set('America/Sao_Paulo');
 // conexão com o banco de dados
 
 define ('BD_SERVIDOR','localhost');
-define ('BD_USUARIO','id18911902_user');
+define ('BD_USUARIO','id18911902_root');
 define ('BD_SENHA','9(P4rfR_[)OLz%-5');
 define ('BD_BANCO','id18911902_livraria');
 
 ?>
+
+
