bd793d25372302fd7a90fa042415f4183c0adf11
LucasAugustoFatec <106602796+LucasAugustoFatec@users.noreply.github.com>
2022-06-07 22:01:38 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php require_once("../model/banco.php");?>
+
+<!DOCTYPE html>
+
+<html lang="pt.br">
+    
+<?php include("head.php"); ?>
+
+<body>
+    <?php include("menu.php")?>
+    <table class="table">
+        <thead>
+            <tr>
+                <!--<th>Nome</th>-->
+                
+                <tr>
+            <td>
+               <font size="3" face="Verdana, Arial, Helvetica, sans-serif">Nome:</font>
+            </td>
+            <td>
+               <font size="2">
+                  <input name="nome" type="text" id="nome" class="formbutton">
+               </font>
+            </td>
+         </tr>
+                
+                
+                
+                
+                <tr>
+                <!--<th>Autor</th>-->
+                
+                 <tr>
+            <td>
+               <font size="3" face="Verdana, Arial, Helvetica, sans-serif">Autor:</font>
+            </td>
+            <td>
+               <font size="2">
+                  <input name="autor" type="text" id="autor" class="formbutton">
+               </font>
+            </td>
+         </tr>
+                
+                
+                
+                <tr>
+                <!--<th>Quantidade de Páginas</th>-->
+                
+                 <tr>
+            <td>
+               <font size="3" face="Verdana, Arial, Helvetica, sans-serif">Quantidade de Páginas:</font>
+            </td>
+            <td>
+               <font size="2">
+                  <input name="qtd_pag" type="text" id="qtd_pag" class="formbutton">
+               </font>
+            </td>
+         </tr>
+                
+                
+                
+                <tr>
+                <!--<th>Preço</th>-->
+                
+                
+                  <td>
+               <font size="3" face="Verdana, Arial, Helvetica, sans-serif">Preço:</font>
+            </td>
+            <td>
+               <font size="2">
+                  <input name="preco" type="text" id="preco" class="formbutton">
+               </font>
+            </td>
+         </tr>
+                
+                
+                
+                
+                <tr>
+                <!--<th>Data</th>-->
+                
+                
+                     <td>
+               <font size="3" face="Verdana, Arial, Helvetica, sans-serif">Data:</font>
+            </td>
+            <td>
+               <font size="2">
+                  <input name="data" type="text" id="data" class="formbutton">
+               </font>
+            </td>
+         </tr>
+                
+                
+                <tr>
+                <!--<th>Flag</th>-->
+                
+                          <td>
+               <font size="3" face="Verdana, Arial, Helvetica, sans-serif">Flag:</font>
+            </td>
+            <td>
+               <font size="2">
+                  <input name="flag" type="text" id="flag" class="formbutton">
+               </font>
+            </td>
+         </tr>
+                
+                
+                
+                <tr>
+                <!--<th>Opções</th>-->
+                
+                               <td>
+               <font size="3" face="Verdana, Arial, Helvetica, sans-serif">Oções:</font>
+            </td>
+            <td>
+               <font size="2">
+                  <input name="opcoes" type="text" id="opcoes" class="formbutton">
+               </font>
+            </td>
+         </tr>
+                
+                
+            </tr>
+        </thead>
+    </table>
+</body>
+
+</html>
