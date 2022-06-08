7287712df161ed6b53d1efb56a774a2b1d6bdb7e
LucasAugustoFatec <106602796+LucasAugustoFatec@users.noreply.github.com>
2022-06-07 22:02:39 -0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,8 @@
+<hr>
+<div class="form-group">
+  <div class="mx-auto" style="width: 1200px;">
+    <a href="cadastro.php" class="btn btn-sucesses">Realizar cadastro desejado</a>
+    <a href="index.php" class="btn btn-default">Realizar consulta</a>
+  </div>
+</div>
+<hr>
